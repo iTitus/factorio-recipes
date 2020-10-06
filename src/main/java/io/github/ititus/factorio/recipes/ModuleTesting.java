84eb4dd58b9537121ab5ac7ba4885901a987209b
iTitus <iTitus@users.noreply.github.com>
2020-10-06 15:22:41 +0200

    cleanup

--- src/main/java/io/github/ititus/factorio/recipes/ModuleTesting.java
+++ src/main/java/io/github/ititus/factorio/recipes/ModuleTesting.java
@@ -8,16 +8,15 @@ import io.github.ititus.factorio.recipes.data.prototype.entity.CraftingMachine;
 import io.github.ititus.factorio.recipes.data.prototype.entity.Lab;
 import io.github.ititus.factorio.recipes.data.prototype.item.Module;
 import io.github.ititus.math.number.BigRational;
-import io.github.ititus.math.number.BigRationalConstants;
 import io.github.ititus.math.permutation.Permutations;
 
 import java.nio.file.Path;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import static io.github.ititus.math.number.BigRationalConstants.ONE;
+import static io.github.ititus.math.number.BigRationalConstants.ZERO;
 
 public class ModuleTesting {
 
@@ -26,8 +25,9 @@ public class ModuleTesting {
     private final PrototypeSet<CraftingMachine> craftingMachines;
 
     public static void main(String[] args) {
-        new ModuleTesting().craftingMachine();
-        //new ModuleTesting().labSpeed();
+        ModuleTesting mt = new ModuleTesting();
+        mt.craftingMachine();
+        mt.labSpeed();
     }
 
     private ModuleTesting() {
@@ -56,11 +56,18 @@ public class ModuleTesting {
         private final BigRational effectiveSpeed;
         private final BigRational demand;
 
-        private MachineStats(List<Module> machineModules, BigRational baseCraftingSpeed, ModuleEffect outerEffect) {
+        private MachineStats(List<Module> machineModules, BigRational baseCraftingSpeed,
+                             BigRational baseProductivityBonus,
+                             BigRational speedBonusThroughTech, BigRational productivityBonusThroughTech,
+                             ModuleEffect beaconEffect) {
             this.modules = machineModules;
-            this.effect =
-                    machineModules.stream().map(Module::getEffect).reduce(ModuleEffect::add).orElse(ModuleEffect.NO_EFFECT).add(outerEffect);
-            this.speed = baseCraftingSpeed.multiply(ONE.add(effect.getSpeedBonus()));
+            this.effect = ModuleEffect.of(machineModules)
+                    .add(beaconEffect)
+                    .add(ModuleEffect.builder().productivity(baseProductivityBonus).build())
+                    .add(ModuleEffect.builder().productivity(productivityBonusThroughTech).build());
+            this.speed = baseCraftingSpeed
+                    .multiply(ONE.add(effect.getSpeedBonus()))
+                    .multiply(ONE.add(speedBonusThroughTech));
             this.effectiveSpeed = speed.multiply(ONE.add(effect.getProductivityBonus()));
             this.demand = ONE.divide(ONE.add(effect.getProductivityBonus()));
         }
@@ -73,8 +80,8 @@ public class ModuleTesting {
     }
 
     private void craftingMachine() {
-        // "electric-furnace"
         printMachineStats("assembling-machine-3", 8);
+        printMachineStats("electric-furnace", 8);
         printMachineStats("chemical-plant", 8);
         printMachineStats("rocket-silo", 20);
     }
@@ -87,88 +94,55 @@ public class ModuleTesting {
 
     private List<MachineStats> getMachineStats(String machineName, int beaconCount) {
         CraftingMachine machine = craftingMachines.get(machineName);
-        //System.out.println(machine);
-        //System.out.println();
+        System.out.println(machine);
 
         Module speed3 = modules.get("speed-module-3");
         Module prod3 = modules.get("productivity-module-3");
 
         BigRational baseCraftingSpeed = machine.getCraftingSpeed();
         BigRational baseProductivityBonus = machine.getBaseProductivity();
-        ModuleEffect beaconEffect = IntStream.range(0, beaconCount)
-                .mapToObj(i -> List.of(speed3, speed3))
-                .flatMap(List::stream)
-                .map(Module::getEffect)
-                .reduce(ModuleEffect::add)
-                .orElse(ModuleEffect.NO_EFFECT)
-                .multiply(BigRationalConstants.ONE_OVER_TWO);
+        ModuleEffect beaconEffect = ModuleEffect.of(speed3, speed3).beaconized(beaconCount);
 
-        ModuleEffect outerEffect =
-                machine.filter(beaconEffect.add(ModuleEffect.builder().productivity(baseProductivityBonus).build()));
-
-        //System.out.println("baseCraftingSpeed=" + baseCraftingSpeed);
-        //System.out.println("baseProductivityBonus=" + baseProductivityBonus);
-        //System.out.println("baseEffectiveCraftingSpeed=" + baseCraftingSpeed.multiply(ONE.add
-        // (baseProductivityBonus)));
-        //System.out.println("beaconEffect=" + beaconEffect);
-        //System.out.println("outerEffect=" + outerEffect);
-        //System.out.println("baseSpeed=" + baseCraftingSpeed.multiply(ONE.add(outerEffect.getSpeedBonus())));
-        //System.out.println("baseEffectiveSpeed=" + baseCraftingSpeed.multiply(ONE.add(outerEffect.getSpeedBonus()))
-        // .multiply(ONE.add(outerEffect.getProductivityBonus())));
-        //System.out.println();
+        BigRational speedBonusThroughTech = ZERO;
+        BigRational productivityBonusThroughTech = ZERO;
 
         int slots = machine.getModuleSpecification().getModuleSlots();
         System.out.println("moduleSlots=" + slots);
 
         List<List<Module>> permutations = Permutations.permute(modules, slots);
-        // permutations.stream().map(l -> CollectionUtil.deepMap(l, Module::getName)).forEachOrdered(System
-        // .out::println);
-
         return permutations.stream()
-                .map(p -> new MachineStats(p, baseCraftingSpeed, outerEffect))
+                .map(p -> new MachineStats(p, baseCraftingSpeed, baseProductivityBonus, speedBonusThroughTech,
+                        productivityBonusThroughTech, beaconEffect))
                 .sorted(Comparator.comparing(o -> o.effectiveSpeed))
                 .collect(Collectors.toUnmodifiableList());
     }
 
     private void labSpeed() {
+        int beaconCount = 0;
+        System.out.println("lab with " + beaconCount + " beacons");
+
         Lab lab = labs.get("lab");
+        System.out.println(lab);
 
         Module speed3 = modules.get("speed-module-3");
         Module prod3 = modules.get("productivity-module-3");
 
-        int beaconCount = 12;
-        ModuleEffect beaconEffect = IntStream.range(0, beaconCount)
-                .mapToObj(i -> List.of(speed3, speed3))
-                .flatMap(List::stream)
-                .map(Module::getEffect)
-                .reduce(ModuleEffect::add)
-                .orElse(ModuleEffect.NO_EFFECT)
-                .multiply(BigRationalConstants.ONE_OVER_TWO);
-
-        // beaconEffect = ModuleEffect.NO_EFFECT;
+        ModuleEffect beaconEffect = ModuleEffect.of(speed3, speed3).beaconized(beaconCount);
 
         BigRational baseResearchSpeed = lab.getResearchingSpeed();
-        BigRational researchSpeedBonusThroughTech = BigRational.of(2.5);
         BigRational baseProductivityBonus = lab.getBaseProductivity();
 
-        ModuleEffect outerEffect = beaconEffect.add(ModuleEffect.builder().productivity(baseProductivityBonus).build());
+        BigRational researchSpeedBonusThroughTech = BigRational.of(2.5);
+        BigRational productivityBonusThroughTech = ZERO;
 
         int slots = lab.getModuleSpecification().getModuleSlots();
+        System.out.println("moduleSlots=" + slots);
 
-        Permutations.permute(modules, slots).stream()
-                .map(p -> new Object() {
-                    List<Module> modules = p;
-                    ModuleEffect effect =
-                            p.stream().map(Module::getEffect).reduce(ModuleEffect::add).orElse(ModuleEffect.NO_EFFECT).add(outerEffect);
-                    BigRational speed =
-                            baseResearchSpeed.multiply(ONE.add(researchSpeedBonusThroughTech)).multiply(ONE.add(effect.getSpeedBonus()));
-                    BigRational effectiveSpeed =
-                            speed.multiply(ONE.add(effect.getProductivityBonus()));
-                    BigRational demand =
-                            ONE.divide(ONE.add(effect.getProductivityBonus()));
-                })
+        List<List<Module>> permutations = Permutations.permute(modules, slots);
+        permutations.stream()
+                .map(p -> new MachineStats(p, baseResearchSpeed, baseProductivityBonus, researchSpeedBonusThroughTech,
+                        productivityBonusThroughTech, beaconEffect))
                 .sorted(Comparator.comparing(o -> o.effectiveSpeed))
-                .map(o -> CollectionUtil.deepMap(o.modules, Module::getName) + ": speedBonus=" + o.effect.getSpeedBonus() + " | productivityBonus=" + o.effect.getProductivityBonus() + " | speed=" + o.speed + " | effSpeed=" + o.effectiveSpeed + " | consumption=" + o.demand)
                 .forEachOrdered(System.out::println);
     }
 }
--- src/main/java/io/github/ititus/factorio/recipes/data/item/module/ModuleEffect.java
+++ src/main/java/io/github/ititus/factorio/recipes/data/item/module/ModuleEffect.java
@@ -1,18 +1,24 @@
 package io.github.ititus.factorio.recipes.data.item.module;
 
 import io.github.ititus.data.Printable;
+import io.github.ititus.factorio.recipes.data.prototype.item.Module;
 import io.github.ititus.factorio.recipes.util.LuaUtil;
+import io.github.ititus.math.number.BigIntegerMath;
 import io.github.ititus.math.number.BigRational;
 import io.github.ititus.math.number.BigRationalConstants;
 import org.luaj.vm2.LuaTable;
 import org.luaj.vm2.LuaValue;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Map;
 import java.util.Objects;
 
 public final class ModuleEffect implements Printable {
 
-    public static final ModuleEffect NO_EFFECT = new ModuleEffect(BigRationalConstants.ZERO, BigRationalConstants.ZERO, BigRationalConstants.ZERO, BigRationalConstants.ZERO);
+    public static final ModuleEffect NO_EFFECT = new ModuleEffect(BigRationalConstants.ZERO,
+            BigRationalConstants.ZERO, BigRationalConstants.ZERO, BigRationalConstants.ZERO);
 
     private final BigRational consumptionBonus, productivityBonus, pollutionBonus, speedBonus;
 
@@ -23,11 +29,20 @@ public final class ModuleEffect implements Printable {
         this.speedBonus = get(t, "speed");
     }
 
-    private ModuleEffect(BigRational consumptionBonus, BigRational productivityBonus, BigRational pollutionBonus, BigRational speedBonus) {
+    private ModuleEffect(BigRational consumptionBonus, BigRational productivityBonus, BigRational pollutionBonus,
+                         BigRational speedBonus) {
         this.consumptionBonus = consumptionBonus;
         this.productivityBonus = productivityBonus;
         this.pollutionBonus = pollutionBonus;
         this.speedBonus = speedBonus;
+    }
+
+    public static ModuleEffect of(Module... modules) {
+        return Arrays.stream(modules).map(Module::getEffect).reduce(NO_EFFECT, ModuleEffect::add);
+    }
+
+    public static ModuleEffect of(Collection<Module> modules) {
+        return modules.stream().map(Module::getEffect).reduce(NO_EFFECT, ModuleEffect::add);
     }
 
     public static Builder builder() {
@@ -36,7 +51,9 @@ public final class ModuleEffect implements Printable {
 
     public static class Builder {
 
-        private BigRational consumptionBonus = BigRationalConstants.ZERO, productivityBonus = BigRationalConstants.ZERO, pollutionBonus = BigRationalConstants.ZERO, speedBonus = BigRationalConstants.ZERO;
+        private BigRational consumptionBonus = BigRationalConstants.ZERO, productivityBonus =
+                BigRationalConstants.ZERO, pollutionBonus = BigRationalConstants.ZERO, speedBonus =
+                BigRationalConstants.ZERO;
 
         public Builder consumption(BigRational consumptionBonus) {
             this.consumptionBonus = consumptionBonus;
@@ -89,11 +106,33 @@ public final class ModuleEffect implements Printable {
     }
 
     public ModuleEffect add(ModuleEffect o) {
+        if (o == NO_EFFECT) {
+            return this;
+        }
         return new ModuleEffect(consumptionBonus.add(o.consumptionBonus).max(BigRationalConstants.MINUS_FOUR_OVER_FIVE), productivityBonus.add(o.productivityBonus), pollutionBonus.add(o.pollutionBonus), speedBonus.add(o.speedBonus));
     }
 
     public ModuleEffect multiply(BigRational r) {
-        return new ModuleEffect(consumptionBonus.multiply(r).max(BigRationalConstants.MINUS_FOUR_OVER_FIVE), productivityBonus.multiply(r), pollutionBonus.multiply(r), speedBonus.multiply(r));
+        if (r.isNegative()) {
+            throw new IllegalArgumentException();
+        } else if (r.isZero()) {
+            return NO_EFFECT;
+        }
+        return new ModuleEffect(consumptionBonus.multiply(r).max(BigRationalConstants.MINUS_FOUR_OVER_FIVE),
+                productivityBonus.multiply(r), pollutionBonus.multiply(r), speedBonus.multiply(r));
+    }
+
+    public ModuleEffect beaconized() {
+        return beaconized(BigInteger.ONE);
+    }
+
+    public ModuleEffect beaconized(int beaconCount) {
+        return beaconized(BigIntegerMath.of(beaconCount));
+    }
+
+    public ModuleEffect beaconized(BigInteger beaconCount) {
+        //TODO: filter out productivity bonus
+        return multiply(BigRational.of(beaconCount, BigInteger.TWO));
     }
 
     @Override
@@ -105,7 +144,8 @@ public final class ModuleEffect implements Printable {
             return false;
         }
         ModuleEffect that = (ModuleEffect) o;
-        return Objects.equals(consumptionBonus, that.consumptionBonus) && Objects.equals(productivityBonus, that.productivityBonus) && Objects.equals(pollutionBonus, that.pollutionBonus) && Objects.equals(speedBonus, that.speedBonus);
+        return Objects.equals(consumptionBonus, that.consumptionBonus) && Objects.equals(productivityBonus,
+                that.productivityBonus) && Objects.equals(pollutionBonus, that.pollutionBonus) && Objects.equals(speedBonus, that.speedBonus);
     }
 
     @Override
