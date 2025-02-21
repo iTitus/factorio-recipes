5788a11534441241ff51880a743a131160f7fc73
iTitus <iTitus@users.noreply.github.com>
2025-02-21 21:38:19 +0100

    update

--- src/main/java/io/github/ititus/factorio/recipes/util/JsonUtil.java
+++ src/main/java/io/github/ititus/factorio/recipes/util/JsonUtil.java
@@ -6,6 +6,7 @@ import org.json.JSONTokener;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UncheckedIOException;
+import java.net.URI;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
@@ -16,10 +17,10 @@ public final class JsonUtil {
 
     public static JSONObject loadJSONFromURL(String url) {
         JSONObject obj;
-        try (InputStreamReader r = new InputStreamReader(new URL(url).openStream(), StandardCharsets.UTF_8)) {
+        try (InputStreamReader r = new InputStreamReader(new URI(url).toURL().openStream(), StandardCharsets.UTF_8)) {
             obj = new JSONObject(new JSONTokener(r));
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
         return obj;
     }
